b2d02984a4dfe245c6296f5d90c0f12e91c0f6b9
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-06-14 22:25:02 +0700

    add data experience

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -1,10 +1,37 @@
 import Layout from "../../components/Layout"
 
+import { data } from "../../datas/experience.json"
+
 const Experience = () => {
     return (
         <Layout>
-            <h1 className="text-3xl font-bold mb-4">Welcome to My Website</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div className="flex flex-col items-center justify-center py-8">
+                <h2 className="text-3xl font-bold mb-6">Working Experience</h2>
+                <div className="max-w-3xl w-full mx-auto">
+                    {data?.map((experience: any, index: number) => (
+                        <div
+                            key={index}
+                            className={`relative flex flex-col ${index % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'
+                                } my-10`}
+                        >
+                            <div className="flex items-center justify-center w-16 h-16 bg-porto-sky-500 rounded-full text-white font-bold text-lg md:absolute md:left-0 md:top-1/2 transform -translate-y-1/2">
+                                {index + 1}
+                            </div>
+                            <div
+                                className={`bg-white p-6 shadow-md rounded-md ${index % 2 === 0 ? 'md:ml-16' : 'md:mr-16'
+                                    }`}
+                            >
+                                <h3 className="text-lg font-bold mb-2">{experience.title}</h3>
+                                <h4 className="text-gray-500 mb-2">{experience.company}</h4>
+                                <p className="text-gray-600 mb-4">{experience.date}</p>
+                                <p className="text-gray-700">{experience.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+
         </Layout>
 
     )
