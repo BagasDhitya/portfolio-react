705bf35af459244c45aad9566a4398a9712166dc
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-06-15 10:48:50 +0700

    add animation on working experience

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -5,13 +5,13 @@ import { data } from "../../datas/experience.json"
 const Experience = () => {
     return (
         <Layout>
-            <div className="flex flex-col items-center justify-center py-8">
-                <h2 className="text-3xl font-bold mb-6">Working Experience</h2>
+            <div className="flex justify-center py-8">
+                <h2 className="text-3xl font-bold">Working Experience</h2>
                 <div className="max-w-3xl w-full mx-auto">
                     {data?.map((experience: any, index: number) => (
                         <div
                             key={index}
-                            className={`relative flex flex-col ${index % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'
+                            className={`relative flex flex-col ${index % 2 === 0 ? 'from-right-animation' : 'from-left-animation'
                                 } my-10`}
                         >
                             <div className="flex items-center justify-center w-16 h-16 bg-porto-sky-500 rounded-full text-white font-bold text-lg md:absolute md:left-0 md:top-1/2 transform -translate-y-1/2">
@@ -31,9 +31,7 @@ const Experience = () => {
                 </div>
             </div>
 
-
         </Layout>
-
     )
 }
 
