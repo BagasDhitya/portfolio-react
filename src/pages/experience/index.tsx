5002e33616e734d26e6b9a7dfe6fb7bbb6cc25e2
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-06-15 11:37:24 +0700

    slicing timeline experience

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -5,21 +5,21 @@ import { data } from "../../datas/experience.json"
 const Experience = () => {
     return (
         <Layout>
-            <div className="flex justify-center py-8">
-                <h2 className="text-3xl font-bold">Working Experience</h2>
+            <div className="flex justify-center py-8 lg:mx-72">
                 <div className="max-w-3xl w-full mx-auto">
                     {data?.map((experience: any, index: number) => (
                         <div
                             key={index}
-                            className={`relative flex flex-col ${index % 2 === 0 ? 'from-right-animation' : 'from-left-animation'
-                                } my-10`}
+                            className={`relative flex flex-col my-10`}
                         >
-                            <div className="flex items-center justify-center w-16 h-16 bg-porto-sky-500 rounded-full text-white font-bold text-lg md:absolute md:left-0 md:top-1/2 transform -translate-y-1/2">
+                            {index >= 0 && (
+                                <div className="absolute bg-gray-300 h-full w-px left-8 top-0" />
+                            )}
+                            <div className={`flex items-center justify-center w-16 h-16 bg-porto-sky-500 rounded-full text-white font-bold text-lg md:absolute md:left-0 md:top-1/2 transform -translate-y-1/2`}>
                                 {index + 1}
                             </div>
                             <div
-                                className={`bg-white p-6 shadow-md rounded-md ${index % 2 === 0 ? 'md:ml-16' : 'md:mr-16'
-                                    }`}
+                                className={`bg-white p-6 shadow-md rounded-md ml-24`}
                             >
                                 <h3 className="text-lg font-bold mb-2">{experience.title}</h3>
                                 <h4 className="text-gray-500 mb-2">{experience.company}</h4>
